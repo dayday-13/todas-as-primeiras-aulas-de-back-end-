91000e44002b7ab1d7605f894f60b052d52532bf
dayday-13 <dayellenn.souza@escola.pr.gov.br>
2026-03-12 09:29:49 -0300

    Add files via upload

---
+++ media_se.js
@@ -0,0 +1,14 @@
+let aluno= "robson"
+let n1=99;
+let n2=100;
+let n3=99;
+let n4=100;
+let media=0;
+let result=""; 
+media=(n1+n2+n3+n4)/4;
+if (media>=70){
+    result="aprovado"
+}else{
+    result="reprovado"
+}
+console.log(" o aluno " +aluno+ " tem média de " +media+ " e está: " +result);
